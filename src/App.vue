2901534d44716802a5dd621b934c24c9054a87c9
rahulbabu7 <rahulbabu436@gmail.com>
2023-07-28 10:59:34 +0530

    card added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
-
+import cardContainer from "./components/cardContainer.vue"
 </script>
 
 <template>
-
+<cardContainer/>
 </template>
 
 <style scoped>
---
+++ src/components/cardContainer.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+<h1>
+    hello world
+</h1>
+
+</template>
+
+<style scoped>
+
+</style>
